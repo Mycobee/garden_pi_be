b04f3519d42726f637017fb0912f4d5a2eb88470
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-01 14:50:04 -0600

    Add garden environment measurments controller and index action

--- app/controllers/api/v1/gardens/env_measurements_controller.rb
+++ app/controllers/api/v1/gardens/env_measurements_controller.rb
@@ -1,3 +1,6 @@
 class Api::V1::Gardens::EnvMeasurementsController < ApplicationController
-	
+	def index
+		garden = Garden.find(params[:id])
+		render json: GardenSerializer.new(garden)	
+	end	
 end
