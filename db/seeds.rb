be35adf5abee81348bc9736cffa1b331d70bf44d
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-01 15:37:37 -0600

    Add initial seed file for garden and env_measurementsg

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+garden = Garden.create!(name: "Backyard Raised Bed", latitude: 42.3601, longitude: -71.0589)
+
+garden.env_measurements.create!(soil_temperature: 55.52, soil_moisture: 85.33, created_at: 105.minutes.ago)
+garden.env_measurements.create!(soil_temperature: 57.28, soil_moisture: 83.22, created_at: 90.minutes.ago)
+garden.env_measurements.create!(soil_temperature: 59.11, soil_moisture: 81.11, created_at: 75.minutes.ago)
+garden.env_measurements.create!(soil_temperature: 59.92, soil_moisture: 78.47, created_at: 60.minutes.ago)
+garden.env_measurements.create!(soil_temperature: 62.43, soil_moisture: 76.82, created_at: 45.minutes.ago)
+garden.env_measurements.create!(soil_temperature: 63.88, soil_moisture: 73.90, created_at: 30.minutes.ago)
+garden.env_measurements.create!(soil_temperature: 65.55, soil_moisture: 71.02, created_at: 15.minutes.ago)
+garden.env_measurements.create!(soil_temperature: 69.33, soil_moisture: 68.86)
