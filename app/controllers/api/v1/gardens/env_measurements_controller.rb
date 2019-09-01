51264724674ec79f85d6dddeb4040e36f20fe476
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-01 15:10:49 -0600

    Add 404 for garden not in db

--- app/controllers/api/v1/gardens/env_measurements_controller.rb
+++ app/controllers/api/v1/gardens/env_measurements_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::Gardens::EnvMeasurementsController < ApplicationController
 	def index
-		garden = Garden.find(params[:id])
+		garden = Garden.find_by(id: params[:id])
+		not_found if garden.nil?
 		render json: GardenSerializer.new(garden)	
 	end	
 end
