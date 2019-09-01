175d4a2ff8b420d543d7df8ef856c63df6e5415e
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-01 17:30:43 -0600

    Add tabbing edit

--- app/serializers/garden_serializer.rb
+++ app/serializers/garden_serializer.rb
@@ -2,11 +2,11 @@ class GardenSerializer < ActiveModel::Serializer
   attributes :id, :name, :latitude, :longitude, :env_measurements
 
 	def env_measurements 
-  	self.object.env_measurements.map do |measurement|
-  		{id: measurement.id,
-			soilMoisture: measurement.soil_moisture, 
-			soilTemperature: measurement.soil_temperature,
-			createdAt: measurement.created_at.strftime("%F %T")}
+		self.object.env_measurements.map do |measurement|
+			{id: measurement.id,
+				soilMoisture: measurement.soil_moisture, 
+				soilTemperature: measurement.soil_temperature,
+				createdAt: measurement.created_at.strftime("%F %T")}
     end 
   end 
 end
