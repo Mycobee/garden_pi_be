df098a5763c055a095de0d76e18669ed1f16fa62
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-01 13:13:58 -0600

    Add autogenerated schema updates from migration

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema.define(version: 2019_09_01_183146) do
 
   create_table "gardens", force: :cascade do |t|
     t.string "name"
-    t.decimal "latitute"
-    t.decimal "longitude"
+    t.float "latitude"
+    t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
