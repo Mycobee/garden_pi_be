bd3add86a9db5d0ae003df5cb5b7459820e4f077
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-01 14:17:45 -0600

    Add namespaced api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :gardens, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	namespace :api do
+    namespace :v1 do
+			namespace :gardens do
+				get '/:id/env_measurements', to: 'env_measurements#index'
+			end
+    end
+  end	
 end
