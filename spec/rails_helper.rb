b576afb53b536ddf897326d457390b3fdbc1d4cd
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-01 15:44:15 -0600

    Add additional filter for auto-generated rails files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -72,6 +72,9 @@ end
 SimpleCov.start 'rails' do
   add_filter '/bin/'
   add_filter '/db/'
+	add_filter '/app/channels'
+	add_filter '/app/jobs'
+	add_filter '/app/mailers'
   add_filter '/spec/' # for rspec
   add_filter '/test/' # for minitest
 end
