656d10bf7bd00565fb05b80d088f6394c250d189
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-01 12:49:48 -0600

    Add gardens factory

--- spec/factories/gardens.rb
+++ spec/factories/gardens.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :garden do
     sequence(:name) { |n| "Garden #{n}" }
-		sequence(:latitude) { |n| 40 - n}
-		sequence(:longitude) { |n| 95 + n}
+		sequence(:latitude) { |n| 40.1234 - n}
+		sequence(:longitude) { |n| 95.4321 + n}
   end
 end
