17c0a2ae8515f8af455f6c3fc7df4f0a14eed1fd
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-01 14:06:47 -0600

    Correct setup data created

--- spec/requests/api/v1/gardens/env_measurements/index_spec.rb
+++ spec/requests/api/v1/gardens/env_measurements/index_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 describe "env_measurements api", type: :request do
 	before :each do
 		@garden = create(:garden)
-		@em_1 = create(:env_measurement, @garden)	
-		@em_2 = create(:env_measurement, @garden)	
-		@em_3 = create(:env_measurement, @garden)	
-		@em_4 = create(:env_measurement, @garden)	
-		@em_5 = create(:env_measurement, @garden)	
-		@em_6 = create(:env_measurement, @garden)	
-		@em_7 = create(:env_measurement, @garden)	
-		@em_8 = create(:env_measurement, @garden)	
+		@em_1 = create(:env_measurement, garden: @garden)	
+		@em_2 = create(:env_measurement, garden:  @garden)	
+		@em_3 = create(:env_measurement, garden:  @garden)	
+		@em_4 = create(:env_measurement, garden:  @garden)	
+		@em_5 = create(:env_measurement, garden:  @garden)	
+		@em_6 = create(:env_measurement, garden:  @garden)	
+		@em_7 = create(:env_measurement, garden:  @garden)	
+		@em_8 = create(:env_measurement, garden:  @garden)	
 	end
 	
 	it "Sends a garden and its associated env_measurements" do
