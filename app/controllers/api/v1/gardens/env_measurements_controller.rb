5027a88fb7cac64b14604dc3ec4ca0185f6455cf
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-02 11:07:21 -0600

    Add updated controller actions for new API design

--- app/controllers/api/v1/gardens/env_measurements_controller.rb
+++ app/controllers/api/v1/gardens/env_measurements_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::Gardens::EnvMeasurementsController < ApplicationController
+  before_action :set_garden, only: [:index]
+
 	def index
-		garden = Garden.find_by(id: params[:id])
-		not_found if garden.nil?
-		render json: GardenSerializer.new(garden)	
+    render json: EnvMeasurementSerializer.new(@garden.env_measurements)	
 	end	
+
+  private
+
+  def set_garden
+    @garden = Garden.find_by(id: params[:id])
+    not_found if @garden.nil?
+  end
 end
