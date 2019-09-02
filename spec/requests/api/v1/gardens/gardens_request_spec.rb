69a8f96e68030aa9d87f2ede84b741145473424b
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-02 12:07:06 -0600

    Add garden show request spec

---
+++ spec/requests/api/v1/gardens/gardens_request_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe "gardens api", type: :request do
+	before :each do
+		@garden = create(:garden)
+	end
+	
+	it "Shows an individual garden" do
+		get "/api/v1/gardens/#{@garden.id}"
+
+		expect(response).to have_http_status(200)
+
+		garden_data = JSON.parse(response.body, symbolize_names: true)[:data]
+  
+    expect(garden_data[:id].to_i).to eq(@garden.id)
+    expect(garden_data[:attributes][:latitude].to_f).to eq(@garden.latitude)
+    expect(garden_data[:attributes][:longitude].to_f).to eq(@garden.longitude)
+	end
+
+	it "returns 404 for a garden not in the DB" do
+		get "/api/v1/gardens/#{@garden.id + 1}/env_measurements"
+
+    expect(response.status).to eq(404)
+    error = JSON.parse(response.body, symbolize_names: true)[:error]
+    expect(error).to eq("Garden Not Found")
+	end
+end
+
+
