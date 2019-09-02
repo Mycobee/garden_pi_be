b1436e3df001fa4a80b43f84b33cf2c33dc51076
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-02 11:19:45 -0600

    Add custom 404 error to avoid exposing stack trace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
 	def not_found
-  		raise ActionController::RoutingError.new('Not Found')
+    render json: { error: "Garden Not Found" }, status: :not_found
 	end
 end
