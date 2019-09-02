569483b27081bd374556de2307639705cfece596
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-02 12:07:41 -0600

    Add gardens controller show action and error handling method

--- app/controllers/api/v1/gardens_controller.rb
+++ app/controllers/api/v1/gardens_controller.rb
@@ -1,3 +1,14 @@
 class Api::V1::GardensController < ApplicationController
-  
+  before_action :set_garden, only: [:show]
+
+	def show
+    render json: GardenSerializer.new(@garden)	
+	end	
+
+  private
+
+  def set_garden
+    @garden = Garden.find_by(id: params[:id])
+    not_found if @garden.nil?  
+  end
 end
