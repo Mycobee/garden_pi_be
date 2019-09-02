1d753a4c86d9ee6766d57a69bc7531b80134a0fc
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-02 10:54:47 -0600

    Add spec for revised API design

--- spec/requests/api/v1/gardens/env_measurements/index_spec.rb
+++ spec/requests/api/v1/gardens/env_measurements/index_spec.rb
@@ -13,14 +13,13 @@ describe "env_measurements api", type: :request do
 		@em_8 = create(:env_measurement, garden: @garden)	
 	end
 	
-	it "Sends a garden and its associated env_measurements" do
+	it "Sends a garden's associated env_measurements" do
 		get "/api/v1/gardens/#{@garden.id}/env_measurements"
 
 		expect(response).to have_http_status(200)
-		garden_data = JSON.parse(response.body, symbolize_names: true)
-		expect(garden_data[:garden][:name]).to eq(@garden.name)
-		expect(garden_data[:garden][:env_measurements].first[:soilTemperature]).to eq(@em_1.soil_temperature)
-		expect(garden_data[:garden][:env_measurements].last[:soilMoisture]).to eq(@em_8.soil_moisture)
+		garden_data = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(garden_data.first[:attributes][:soil_temperature]).to eq(@em_1.soil_temperature)
+    expect(garden_data.last[:attributes][:soil_moisture]).to eq(@em_8.soil_moisture)
 	end
 
 	it "returns 404 for a garden not in the DB" do
