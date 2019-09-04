0482d783deec8568075e4b10dccc43f742e59dcd
Brian Plantico <bplantico@gmail.com>
2019-09-04 14:34:45 -0600

    Adds numericality validations to soil_temperature and soil_moisture in EnvMeasurement model

--- app/models/env_measurement.rb
+++ app/models/env_measurement.rb
@@ -1,3 +1,6 @@
 class EnvMeasurement < ApplicationRecord
 	belongs_to :garden
+
+	validates_numericality_of :soil_temperature
+	validates_numericality_of :soil_moisture, greater_than_or_equal_to: 0
 end
