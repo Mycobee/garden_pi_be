ffdf02c06cbf32f5931aa7933fd88a31461b22e7
billy urrutia <billyurrutia@gmail.com>
2019-09-07 14:36:02 -0600

    Created index for displaying the job and Create for creating the job
    Co-authored-by: Brian Plantico <bplantico@gmail.com>

---
+++ app/controllers/api/v1/gardens/jobs_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::Gardens::JobsController < ApplicationController
+  before_action :set_garden, only: [:index, :create]
+
+	def index
+    render json: JobSerializer.new(@garden.jobs)
+	end
+
+  def create
+    job = Job.new(job_params)
+    job.garden_id = @garden.id
+    if job.save
+      render json: JobSerializer.new(job), status: :created
+    else
+      render json: "{ Job failed to record. }", status: :bad_request
+    end
+  end
+
+  private
+
+  def job_params
+    params.permit(:name)
+  end
+
+  def set_garden
+    @garden = Garden.find_by(id: params[:id])
+    not_found if @garden.nil?
+  end 
+end
