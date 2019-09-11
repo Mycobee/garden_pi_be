de5553912564acd94c8d50392927654f460d3d11
Brian Plantico <bplantico@gmail.com>
2019-09-11 11:52:28 -0600

    Removes comment and unused variable assignment from Garden model

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -6,8 +6,8 @@ class Garden < ApplicationRecord
 	has_many :jobs
 
 	def daily_avg_moisture(days)
-		x = env_measurements.where("created_at >= ? AND created_at <= ?",
-			Time.now.beginning_of_day - (days * 86400),
+		env_measurements.where("created_at >= ? AND created_at <= ?",
+			Time.now.beginning_of_day - (days.to_i * 86400),
 			Time.now.beginning_of_day)
 		.group("DATE_TRUNC('day', created_at)")
 		.average(:soil_moisture)
