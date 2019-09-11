ca3f92925bbde42af50fb43e95ebe5ee7619d857
Brian Plantico <bplantico@gmail.com>
2019-09-11 10:41:41 -0600

    Adds ability to call instance method daily_avg_moisture(days) on a Garden which returns the daily average for the last x days, not including today

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -4,4 +4,13 @@ class Garden < ApplicationRecord
 	# relationships
 	has_many :env_measurements
 	has_many :jobs
+
+	def daily_avg_moisture(days)
+		x = env_measurements.where("created_at >= ? AND created_at <= ?",
+			Time.now.beginning_of_day - (days * 86400),
+			Time.now.beginning_of_day)
+		.group("DATE_TRUNC('day', created_at)")
+		.average(:soil_moisture)
+	end
+
 end
--- spec/models/garden_spec.rb
+++ spec/models/garden_spec.rb
@@ -5,4 +5,77 @@ RSpec.describe Garden, type: :model do
 		it { should have_many :env_measurements }
 		it { should have_many :jobs }
 	end
+
+	describe "#daily_avg" do
+		before :each do
+			@garden = create(:garden)
+
+			today = Time.zone.now.beginning_of_day
+
+			one_day_ago    = today          - 86400
+			two_days_ago   = one_day_ago    - 86400
+			three_days_ago = two_days_ago   - 86400
+			four_days_ago  = three_days_ago - 86400
+			five_days_ago  = four_days_ago  - 86400
+			six_days_ago   = five_days_ago  - 86400
+			seven_days_ago = six_days_ago   - 86400
+			eight_days_ago = seven_days_ago - 86400
+			x = 0
+
+			until today == one_day_ago
+			  @garden.env_measurements.create!(soil_temperature: 10, soil_moisture: 10, created_at: x.minutes.ago)
+			  today = today - 900
+			  x = x + 15
+			end
+
+			until one_day_ago == two_days_ago
+			  @garden.env_measurements.create!(soil_temperature: 20, soil_moisture: 20, created_at: x.minutes.ago)
+			  one_day_ago = one_day_ago - 900
+			  x = x + 15
+			end
+
+			until two_days_ago == three_days_ago
+			  @garden.env_measurements.create!(soil_temperature: 30, soil_moisture: 30, created_at: x.minutes.ago)
+			  two_days_ago = two_days_ago - 900
+			  x = x + 15
+			end
+
+			until three_days_ago == four_days_ago
+			  @garden.env_measurements.create!(soil_temperature: 40, soil_moisture: 40, created_at: x.minutes.ago)
+			  three_days_ago = three_days_ago - 900
+			  x = x + 15
+			end
+
+			until four_days_ago == five_days_ago
+			  @garden.env_measurements.create!(soil_temperature: 50, soil_moisture: 50, created_at: x.minutes.ago)
+			  four_days_ago = four_days_ago - 900
+			  x = x + 15
+			end
+
+			until five_days_ago == six_days_ago
+			  @garden.env_measurements.create!(soil_temperature: 60, soil_moisture: 60, created_at: x.minutes.ago)
+			  five_days_ago = five_days_ago - 900
+			  x = x + 15
+			end
+
+			until six_days_ago == seven_days_ago
+			  @garden.env_measurements.create!(soil_temperature: 70, soil_moisture: 70, created_at: x.minutes.ago)
+			  six_days_ago = six_days_ago - 900
+			  x = x + 15
+			end
+
+			until seven_days_ago == eight_days_ago
+			  @garden.env_measurements.create!(soil_temperature: 80, soil_moisture: 80, created_at: x.minutes.ago)
+			  seven_days_ago = seven_days_ago - 900
+			  x = x + 15
+			end
+    end
+
+		it "returns daily average moisture for given number of days" do
+			expect(@garden.daily_avg_moisture(1).count).to eq(1)
+			expect(@garden.daily_avg_moisture(2).count).to eq(2)
+			expect(@garden.daily_avg_moisture(3).count).to eq(3)
+			expect(@garden.daily_avg_moisture(7).count).to eq(7)
+		end
+	end
 end
