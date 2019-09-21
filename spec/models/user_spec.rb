78b5a0602e145c3125eb5a4c9d9de7e3e8aec34a
Brian Plantico <bplantico@gmail.com>
2019-09-21 17:25:53 -0600

    Adds additional validation to User model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
     it { should validate_presence_of :password }
+    it { should validate_presence_of :api_key }
     it { should validate_uniqueness_of :api_key }
   end
 end
