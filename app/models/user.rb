bd971316bdb31d961c10f308e221d1758f1a88a8
Brian Plantico <bplantico@gmail.com>
2019-09-21 17:21:58 -0600

    Adds set_api_key class method to the User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_create :set_api_key
   has_secure_password
 
   # validations
@@ -7,4 +8,10 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates_presence_of :password
   validates :api_key, presence: true, uniqueness: true
+
+  private
+
+  def set_api_key
+    self.api_key = SecureRandom.urlsafe_base64.to_s
+  end
 end
