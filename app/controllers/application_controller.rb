68e65423ad4b2b181a212c5adc060594089618a0
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-24 13:57:32 -0600

    Add bad_request helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::API
   def unauthorized
     render json: { error: "Invalid Credentials" }, status: :unauthorized
   end
+
+  def bad_request
+    render json: { error: "Bad Request" }, status: :bad_request
+  end
 end
