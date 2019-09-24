e4faab95dad8895f5ed100fa7b4455e2cd036c9a
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-24 13:16:56 -0600

    Add unauthorized method, and rename not_found for clarity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::API
-	def not_found
+	def garden_not_found
     render json: { error: "Garden Not Found" }, status: :not_found
 	end
+
+  def unauthorized
+    render json: { error: "Invalid Credentials" }, status: :unauthorized
+  end
 end
