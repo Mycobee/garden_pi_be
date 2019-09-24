e43ae55d43af3daeda1625b3ea6545c106a46fb2
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-24 12:48:14 -0600

    Add initial garden create action

--- app/controllers/api/v1/gardens_controller.rb
+++ app/controllers/api/v1/gardens_controller.rb
@@ -5,7 +5,18 @@ class Api::V1::GardensController < ApplicationController
     render json: GardenSerializer.new(@garden)	
 	end	
 
+  def create
+    auth_key = request.headers["Authorization"]
+    user = User.find_by(api_key: auth_key)
+    garden = user.gardens.create(garden_params) 
+    render json: GardenSerializer.new(garden), status: :created
+  end
+
   private
+
+  def garden_params
+    params.require(:garden).permit(:name, :latitude, :longitude, :max_moisture, :min_moisture, :auto_water)
+  end
 
   def set_garden
     @garden = Garden.find_by(id: params[:id])
