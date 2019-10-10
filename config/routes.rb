b926498b6f6e00175a25f32e86094da454b71176
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-10-10 16:31:27 -0600

    Add gardens index route

--- config/routes.rb
+++ config/routes.rb
@@ -4,22 +4,22 @@ Rails.application.routes.draw do
 
 	namespace :api do
     	namespace :v1 do
-			namespace :gardens do
-				get '/:id/env_measurements', to: 'env_measurements#index'
-        		post '/:id/env_measurements', to: 'env_measurements#create'
+			  namespace :gardens do
+			  	get '/:id/env_measurements', to: 'env_measurements#index'
+          		post '/:id/env_measurements', to: 'env_measurements#create'
 
-        		get '/:id/jobs', to: 'jobs#index'
-        		post '/:id/jobs', to: 'jobs#create'
+          		get '/:id/jobs', to: 'jobs#index'
+          		post '/:id/jobs', to: 'jobs#create'
 
-				get '/:id/daily_avg_moisture', to: 'daily_avg_moisture#index'
-			end
+			  	get '/:id/daily_avg_moisture', to: 'daily_avg_moisture#index'
+			  end
 
-			resources :gardens, only: [:show, :create]
+			  resources :gardens, only: [:show, :index, :create]
 
-			post '/sessions', to: 'sessions#create'
-			delete '/sessions', to: 'sessions#destroy'
-			  
-			resources :users, only: :create
+			  post '/sessions', to: 'sessions#create'
+			  delete '/sessions', to: 'sessions#destroy'
+			    
+			  resources :users, only: :create
     	end
   	end
 end
