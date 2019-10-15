99d4b9034e20f7cdcedaeb753bfb2a45af087c2e
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-10-15 10:17:24 -0600

    Add welcome message for root path

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ActionController::Base
   def index
+    render json: "Welcome to the Garden Pi api, you can view the documentation at https://github.com/Mycobee/garden_pi_be"
   end
 end
